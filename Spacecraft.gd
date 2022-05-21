f3da3562946878f068bd7ac92a39c6dc0c91d7a3
Difint <yarm-public@protonmail.com>
2022-05-22 02:12:01 +0700

    - Showing exhause

--- Spacecraft.gd
+++ Spacecraft.gd
@@ -1,16 +1,18 @@
 extends KinematicBody
 
 # How fast the player moves in meters per second.
-export var speed = 140.1
+export var speed = 14.1
 # The downward acceleration when in the air, in meters per second squared.
 export var fall_acceleration = 1.6
 
 var velocity = Vector3.ZERO
+var direction = Vector3.ZERO
 
 var speed_up = 100
 
+
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	
 	var throttle = 0
 	
 	if Input.is_action_pressed("yaw_right"):
@@ -24,8 +26,10 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("throttle"):
 		throttle = -1
-		
-		
+		$Pivot/Exhause.visible = true
+	else:
+		$Pivot/Exhause.visible = false
+	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
