e0c06e3c9578f16782053a8af44e2e7df8da3298
The Kicker <cav.lemasters@geneva.edu>
2022-06-01 15:07:31 -0400

    Minor changes, added text shadowing

--- game/ui/Stopwatch.gd
+++ game/ui/Stopwatch.gd
@@ -4,8 +4,7 @@ var time_elapsed := 0.0
 
 func _process(delta: float) -> void:
 	time_elapsed += delta
-	var minutes := time_elapsed / 60
 	var seconds := fmod(time_elapsed, 60)
-	# $Label.set(text, "%02d:%02d" % [minutes, seconds])
-	# $Label.Text = "%02d:%02d" % [minutes, seconds]
-	text = "MET: %02d:%02d" % [minutes, seconds]
+	var minutes := time_elapsed / 60
+	var hours := minutes / 60
+	text = "%02d:%02d:%02d" % [hours, minutes, seconds]
