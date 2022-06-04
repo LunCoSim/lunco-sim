6dc669b456f8ea2b2a534f46f1e23f15a0dddc86
Difint <yarm-public@protonmail.com>
2022-06-04 23:19:59 +0700

    - Implemented Model browser in Main Menu

---
+++ game/main-menu/main-menu.gd
@@ -0,0 +1,32 @@
+extends VBoxContainer
+
+var STUB = "res://views/stub-view.tscn"
+
+
+func _on_Spaceflight_pressed():
+	get_tree().change_scene("res://views/space-flight.tscn")
+
+func _on_ModelBrowser_pressed():
+	get_tree().change_scene("res://views/models-preview.tscn")
+
+func _on_Trajectory_Planning_pressed():
+	get_tree().change_scene(STUB)
+
+func _on_Surface_Operations_pressed():
+	get_tree().change_scene(STUB)
+
+func _on_Lunar_Base_Sim_pressed():
+	get_tree().change_scene(STUB)
+
+func _on_Help_pressed():
+	OS.shell_open("https://github.com/LunCoSim/lunco-sim")
+
+func _on_Settings_pressed():
+	get_tree().change_scene(STUB)
+
+func _on_Exit_pressed():
+	get_tree().quit() # default behavior
+	
+
+func _on_Return_To_Main_Menu_pressed():
+	get_tree().change_scene("res://main-menu/main-menu.tscn")
