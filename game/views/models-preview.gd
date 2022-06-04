b33d8cb2823ce61544851cf5fe46438d19d6cab3
Difint <yarm-public@protonmail.com>
2022-06-04 22:10:01 +0700

    - Creating list of models

---
+++ game/views/models-preview.gd
@@ -0,0 +1,47 @@
+extends Node
+
+#SRC: https://godotengine.org/qa/5175/how-to-get-all-the-files-inside-a-folder
+#Listing all files
+func list_files_in_directory(path, files=[]):
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin(true, true) #filtering files
+
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		else:
+			if dir.dir_exists(file):
+				list_files_in_directory(path+file+"/", files)
+			files.append({
+				"path": path, 
+				"filename": file,
+				"extension": file.split(".")[-1]
+			})
+			
+	dir.list_dir_end()
+
+	return files
+
+func filter_by_extension(files: Array, extension="escn"):
+	var res = []
+	for file in files:
+		if(file["extension"]==extension):
+			res.append(file)
+	return res
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#pass # Replace with function body.
+	var path = "res://addons/"
+	var files = list_files_in_directory(path)
+	
+	var models = filter_by_extension(files)
+	
+	for file in models:
+		print(file)
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
