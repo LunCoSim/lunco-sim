c99ce58778f47ad9a19da205fbbfb1e66663812f
Difint <yarm-public@protonmail.com>
2022-06-14 18:04:40 +0700

    - Describing basic LunCo classes inspired by XTCE

---
+++ game/lunco/command.gd
@@ -0,0 +1,4 @@
+class_name Command
+	
+extends Node
+
---
+++ game/lunco/space-system.gd
@@ -0,0 +1,16 @@
+# This is the basic class according to XTCE
+class_name SpaceSystem
+extends Node #TODO: Check if should be node, maybe something else?
+
+#Basic parameters inspired by XTCE
+export var Commands = {}
+export var Telemetry = {}
+export var Parameters = {}
+
+#----------------
+export var State = {} # Hierarchical state machine
+export var Behaviour = {} # Behaviour tree
+
+#----------------
+func process_command(command: Command) -> bool:
+	return true
