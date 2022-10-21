566fc677c9ca5c26d761f9666bdff37a6d681512
Difint <yarm-public@protonmail.com>
2022-10-21 13:44:44 +0700

    - Moving camera using mouse

--- src/world.gd
+++ src/world.gd
@@ -1,15 +1,36 @@
 extends Node
 
+var mouse_control = false
+const MOUSE_SENSITIVITY = 1.0
+var camera_dir := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+#	if Input.is_action_just_pressed("select_player"):
+##		state.set_trigger("player")
+#		pass
+#	elif Input.is_action_just_pressed("select_spacecraft"):
+#		state.set_trigger("spacecraft")
+#	elif Input.is_action_just_pressed("select_operator"):
+#		state.set_trigger("operator")
+		
+	if Input.is_action_pressed("rotate_camera"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		mouse_control = true
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		mouse_control = false
+	
+	if (event is InputEventMouseMotion) and mouse_control:
+		camera_dir = event.relative * MOUSE_SENSITIVITY
+	else:
+		camera_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var character: LcCharacter = $Character
-	
 	var camera: LcSpringArmCamera = $Camera
 	
 	if Input.is_action_just_pressed("ui_accept"):
@@ -17,10 +38,9 @@ func _process(delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if input_dir.length() > 0:
-		camera.execute_command_args(LcSpringArmCamera.Commands.START_ROTATION, input_dir)
+	if camera_dir.length() > 0:
+		camera.execute_command_args(LcSpringArmCamera.Commands.START_ROTATION, camera_dir)
 	else:
 		camera.execute_command(LcSpringArmCamera.Commands.STOP_ROTATION)
 	
