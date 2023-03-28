249f66bac32af7951d5c6f58ba1756935a2f5c6b
Difint <yarm-public@protonmail.com>
2023-03-28 16:52:51 +0700

    - Updated player model

--- core/controllers/player/player.gd
+++ core/controllers/player/player.gd
@@ -101,91 +101,91 @@ func _ready():
 
 func _process(delta):
 	#TBD: add tolerance timer + probably move to physics
-	if $State:
-		$State.set_param("on_floor", is_on_floor())
-				
-		match state.get_current():
-			"Idle":
-
-				# Aim to zero (no aiming while walking).
-				animation_tree["parameters/aim/add_amount"] = 0
-				# Change state to walk.
-				animation_tree["parameters/state/current"] = 1
-				# Blend position for walk speed based on motion.
-				animation_tree["parameters/walk/blend_position"] = Vector2(motion.length(), 0)
-				
-			"Move":
-				pass
-			"Jump":
-				velocity.y = JUMP_SPEED
-				state.set_trigger("on_air")
-				# Increase airborne time so next frame on_air is still true
-				animation_tree["parameters/state/current"] = 2
-	#			sound_effect_jump.play()
-			"On Air":
-				if (velocity.y > 0):
-					animation_tree["parameters/state/current"] = 2
-				else:
-					animation_tree["parameters/state/current"] = 3
-			"Landed":
-	#			sound_effect_land.play()
-				pass
-			"Aiming":
-				# Change state to strafe.
-				animation_tree["parameters/state/current"] = 0
-				
-	#			# Change aim according to camera rotation.
-	#			if camera_x_rot >= 0: # Aim up.
-	#				animation_tree["parameters/aim/add_amount"] = -camera_x_rot / deg2rad(camera_node.CAMERA_X_ROT_MAX)
-	#			else: # Aim down.
-	#				animation_tree["parameters/aim/add_amount"] = camera_x_rot / deg2rad(camera_node.CAMERA_X_ROT_MIN)
-	#
-	#			# Convert orientation to quaternions for interpolating rotation.
-	#			var q_from = orientation.basis.get_rotation_quat()
-	#			var q_to = camera_node.get_rotation_quat()#TODO: remove somehow, this value should be updated only during aiming
-	#			# Interpolate current rotation with desired one.
-	#			orientation.basis = Basis(q_from.slerp(q_to, delta * ROTATION_INTERPOLATE_SPEED))
-	#
-	#			# The animation's forward/backward axis is reversed.
-	#			animation_tree["parameters/strafe/blend_position"] = Vector2(motion.x, -motion.y)
-	#
-	#			root_motion = animation_tree.get_root_motion_transform()
-			"Shoot":
-	#			var shoot_origin = shoot_from.global_transform.origin
-
-				var ch_pos = crosshair.rect_position + crosshair.rect_size * 0.5
-				#TODO: project ray from Player's model
-	#			var ray_from = camera_node.project_ray_origin(ch_pos)
-	#			var ray_dir = camera_node.project_ray_normal(ch_pos)
-	#
-	#			var shoot_target
-	#			var col = get_world().direct_space_state.intersect_ray(ray_from, ray_from + ray_dir * 1000, [self], 0b11)
-	#			if col.empty():
-	#				shoot_target = ray_from + ray_dir * 1000
-	#			else:
-	#				shoot_target = col.position
-	#			var shoot_dir = (shoot_target - shoot_origin).normalized()
-	#
-	#			var bullet = preload("res://player/bullet/bullet.tscn").instance()
-	#			get_parent().add_child(bullet)
-	#			bullet.global_transform.origin = shoot_origin
-	#			# If we don't rotate the bullets there is no useful way to control the particles ..
-	#			bullet.look_at(shoot_origin + shoot_dir, Vector3.UP)
-	#			bullet.add_collision_exception_with(self)
-	#			var shoot_particle = $PlayerModel/Robot_Skeleton/Skeleton/GunBone/ShootFrom/ShootParticle
-	#			shoot_particle.restart()
-	#			shoot_particle.emitting = true
-	#			var muzzle_particle = $PlayerModel/Robot_Skeleton/Skeleton/GunBone/ShootFrom/MuzzleFlash
-	#			muzzle_particle.restart()
-	#			muzzle_particle.emitting = true
-	#			fire_cooldown.start()
-	#			sound_effect_shoot.play()
-				
-	#			camera_node.add_camera_shake_trauma(0.35)# Todo: Emit signanl shot and connect to camera
+	
+	$State.set_param("on_floor", is_on_floor())
+			
+#	match state.get_current():
+#		"Idle":
+#
+#			# Aim to zero (no aiming while walking).
+#			animation_tree["parameters/aim/add_amount"] = 0
+#			# Change state to walk.
+#			animation_tree["parameters/state/current"] = 1
+#			# Blend position for walk speed based on motion.
+#			animation_tree["parameters/walk/blend_position"] = Vector2(motion.length(), 0)
+#
+#		"Move":
+#			pass
+#		"Jump":
+#			velocity.y = JUMP_SPEED
+#			state.set_trigger("on_air")
+#			# Increase airborne time so next frame on_air is still true
+#			animation_tree["parameters/state/current"] = 2
+##			sound_effect_jump.play()
+#		"On Air":
+#			if (velocity.y > 0):
+#				animation_tree["parameters/state/current"] = 2
+#			else:
+#				animation_tree["parameters/state/current"] = 3
+#		"Landed":
+##			sound_effect_land.play()
+#			pass
+#		"Aiming":
+#			# Change state to strafe.
+#			animation_tree["parameters/state/current"] = 0
+#
+##			# Change aim according to camera rotation.
+##			if camera_x_rot >= 0: # Aim up.
+##				animation_tree["parameters/aim/add_amount"] = -camera_x_rot / deg2rad(camera_node.CAMERA_X_ROT_MAX)
+##			else: # Aim down.
+##				animation_tree["parameters/aim/add_amount"] = camera_x_rot / deg2rad(camera_node.CAMERA_X_ROT_MIN)
+##
+##			# Convert orientation to quaternions for interpolating rotation.
+##			var q_from = orientation.basis.get_rotation_quat()
+##			var q_to = camera_node.get_rotation_quat()#TODO: remove somehow, this value should be updated only during aiming
+##			# Interpolate current rotation with desired one.
+##			orientation.basis = Basis(q_from.slerp(q_to, delta * ROTATION_INTERPOLATE_SPEED))
+##
+##			# The animation's forward/backward axis is reversed.
+##			animation_tree["parameters/strafe/blend_position"] = Vector2(motion.x, -motion.y)
+##
+##			root_motion = animation_tree.get_root_motion_transform()
+#		"Shoot":
+##			var shoot_origin = shoot_from.global_transform.origin
+#
+#			var ch_pos = crosshair.rect_position + crosshair.rect_size * 0.5
+#			#TODO: project ray from Player's model
+##			var ray_from = camera_node.project_ray_origin(ch_pos)
+##			var ray_dir = camera_node.project_ray_normal(ch_pos)
+##
+##			var shoot_target
+##			var col = get_world().direct_space_state.intersect_ray(ray_from, ray_from + ray_dir * 1000, [self], 0b11)
+##			if col.empty():
+##				shoot_target = ray_from + ray_dir * 1000
+##			else:
+##				shoot_target = col.position
+##			var shoot_dir = (shoot_target - shoot_origin).normalized()
+##
+##			var bullet = preload("res://player/bullet/bullet.tscn").instance()
+##			get_parent().add_child(bullet)
+##			bullet.global_transform.origin = shoot_origin
+##			# If we don't rotate the bullets there is no useful way to control the particles ..
+##			bullet.look_at(shoot_origin + shoot_dir, Vector3.UP)
+##			bullet.add_collision_exception_with(self)
+##			var shoot_particle = $PlayerModel/Robot_Skeleton/Skeleton/GunBone/ShootFrom/ShootParticle
+##			shoot_particle.restart()
+##			shoot_particle.emitting = true
+##			var muzzle_particle = $PlayerModel/Robot_Skeleton/Skeleton/GunBone/ShootFrom/MuzzleFlash
+##			muzzle_particle.restart()
+##			muzzle_particle.emitting = true
+##			fire_cooldown.start()
+##			sound_effect_shoot.play()
+#
+##			camera_node.add_camera_shake_trauma(0.35)# Todo: Emit signanl shot and connect to camera
 		
-		root_motion = animation_tree.get_root_motion_transform()
-		#rotating model to match movement direction
-		player_model.global_transform.basis = orientation.basis
+	root_motion = animation_tree.get_root_motion_position()
+	#rotating model to match movement direction
+#	player_model.global_transform.basis = orientation.basis
 	
 func _physics_process(delta):
 	
@@ -210,14 +210,14 @@ func _physics_process(delta):
 #	# Apply root motion to orientation.
 	orientation *= root_motion
 
-	var h_velocity = orientation.origin / delta
-	velocity.x = h_velocity.x
-	velocity.z = h_velocity.z
-	
+#	var h_velocity = orientation.origin / delta
+#	velocity.x = h_velocity.x
+#	velocity.z = h_velocity.z
+#
 	velocity += gravity * delta #TODO: Move to gravity vector
 	
-	orientation.origin = Vector3() # Clear accumulated root motion displacement (was applied to speed).
-	orientation = orientation.orthonormalized() # Orthonormalize orientation.
+#	orientation.origin = Vector3() # Clear accumulated root motion displacement (was applied to speed).
+#	orientation = orientation.orthonormalized() # Orthonormalize orientation.
 	
 	move_and_slide()
 
