d729f2195fb88a6b1f5553ae1b9e15e03e71eb8b
Difint <yarm-public@protonmail.com>
2023-04-03 19:37:08 +0700

    - getting ready for server applications

--- main.gd
+++ main.gd
@@ -5,6 +5,12 @@ var peer = ENetMultiplayerPeer.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+	if ("--server" in OS.get_cmdline_args()) or (OS.has_feature("server")):
+		# Run your server startup code here...
+		# Using this check, you can start a dedicated server by running
+		# a Godot binary (headless or not) with the `--server` command-line argument.
+		_on_sim_host_pressed()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
