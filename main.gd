b3a4ce33af587fbe6f9d9743eedf3461333ac95a
Difint <yarm-public@protonmail.com>
2023-04-11 13:31:33 +0700

    - updated main menu

--- main.gd
+++ main.gd
@@ -4,9 +4,11 @@ var peer = ENetMultiplayerPeer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("Main ready")
 	pass # Replace with function body.
 	
 	if ("--server" in OS.get_cmdline_args()) or (OS.has_feature("server")):
+		print("Running in server mode")
 		# Run your server startup code here...
 		# Using this check, you can start a dedicated server by running
 		# a Godot binary (headless or not) with the `--server` command-line argument.
@@ -19,20 +21,31 @@ func _process(delta):
 
 ## UI Integrations
 func _on_sim_host_pressed():
+	print("_on_sim_host_pressed")
 	peer.create_server(9000)
 	multiplayer.multiplayer_peer = peer
 	
 	get_tree().change_scene_to_file("res://apps/sim/app.tscn")
 
 func _on_sim_client_pressed():
+	print("_on_sim_client_pressed")
 	var ip = %IP.text
 	var port = %Port.text.to_int()
 	
-	peer.create_client(ip, port)
-	multiplayer.multiplayer_peer = peer
+	connect_to_server(ip, port)
 	
 	get_tree().change_scene_to_file("res://apps/sim/app.tscn")
 	pass # Replace with function body.
 	
 func _on_yarm_pressed():
 	get_tree().change_scene_to_file("res://apps/yarm/app.tscn")
+
+
+func _on_connect_to_global_pressed():
+	#default global server
+	connect_to_server("langrenus.lunco.space", 9000)
+	get_tree().change_scene_to_file("res://apps/sim/app.tscn")
+
+func connect_to_server(ip: String, port: int):
+	peer.create_client(ip, port)
+	multiplayer.multiplayer_peer = peer
