797480b2e87ac55e69ea9731c886bc300b3637a2
Difint <yarm-public@protonmail.com>
2023-04-16 19:19:48 +0700

    - added exhaust to Starship

---
+++ core/entities/starship-entity.gd
@@ -0,0 +1,16 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_spacecraft_controller_thrusted(enabled):
+	$Exhaust.visible = enabled
+			
