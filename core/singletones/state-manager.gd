1baab7536980d3075676f0d49426fcea57e96d5b
Difint <yarm-public@protonmail.com>
2023-05-19 15:10:20 +0700

    - added state manager

---
+++ core/singletones/state-manager.gd
@@ -0,0 +1,19 @@
+class_name lsStateManager
+extends Node
+
+@export var Username := ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _save():
+	pass
+	
+func _load():
+	pass
--- main.gd
+++ main.gd
@@ -21,6 +21,9 @@ func _process(delta):
 
 ## UI Integrations
 func _on_sim_host_pressed():
+	
+	StateManager.Username = %Username.text
+	
 	print("[INFO] _on_sim_host_pressed")
 	peer.create_server(9000)
 	multiplayer.multiplayer_peer = peer
