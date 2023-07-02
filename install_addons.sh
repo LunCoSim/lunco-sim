4a582f1b4390da4186e84dca95680381ee4f6d90
Difint <yarm-public@protonmail.com>
2023-07-02 22:54:54 +0700

    - Switching addons management to gd-plug

---
+++ install_addons.sh
@@ -0,0 +1,2 @@
+#based on gd-plug guide: https://github.com/imjp94/gd-plug/tree/master#getting-started
+godot --no-window -s plug.gd install 
---
+++ plug.gd
@@ -0,0 +1,26 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+	plug("timothyqiu/godot-csv-data-importer")
+	plug("imjp94/gd-plug-ui")
+	plug("bitbrain/beehave")
+	plug("elenakrittik/GodotXML")
+	plug("folt-a/godot-idea-board")
+	plug("imjp94/gd-YAFSM")
+	plug("Ark2000/PankuConsole")
+#    # Declare your plugins in here with plug(src, args)
+#    # By default, only "addons/" directory will be installed
+#    # Proudly made by me too!
+#    plug("imjp94/UIDesignTool")
+#    plug("imjp94/gd-YAFSM")
+#
+#    # Tools
+#    plug("fenix-hub/godot-engine.github-integration")
+#    plug("EricEzaM/godot-color-palette")
+#
+#    # Unit test
+#    plug("bitwes/Gut")
+#
+#    # By Zylann
+#    plug("Zylann/godot_scatter_plugin")
+#    plug("Zylann/godot_heightmap_plugin")
