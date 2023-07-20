64a1a37591504ec60c93199941d04cbddbe62afe
Difint <yarm-public@protonmail.com>
2023-07-20 15:42:52 +0700

    - Basic QuadSphere for Planetary Scale

---
+++ apps/future_lunar_missions/QuadSphere.gd
@@ -0,0 +1,77 @@
+@tool
+extends MeshInstance3D
+
+@export var Subdivisions: int = 2: set = _update_subdivsions
+@export var Radius: float = 1.0: set = _update_radius
+
+func _ready():
+	_update_mesh()
+
+func _update_subdivsions(value):
+	Subdivisions = value
+	_update_mesh()
+	
+func _update_radius(value):
+	Radius = value
+	_update_mesh()
+
+func _update_mesh():
+	mesh = create_quad_sphere(Subdivisions, Radius)
+	
+func create_quad_sphere(subdivisions:int, radius:float):
+	var baseCube = BoxMesh.new()
+	baseCube.subdivide_depth = subdivisions
+	baseCube.subdivide_height = subdivisions
+	baseCube.subdivide_width = subdivisions
+
+	var mesh = ArrayMesh.new()
+	var surfaces = baseCube.get_surface_count()
+	
+	print("Surfaces: ", surfaces)
+	
+	for i in range(surfaces):
+		var array = baseCube.surface_get_arrays(i)
+		var vertices = array[Mesh.ARRAY_VERTEX]
+		var uvs = array[Mesh.ARRAY_TEX_UV]
+		var normals = array[Mesh.ARRAY_NORMAL]
+		var colors = []
+
+		for j in range(vertices.size()):
+			vertices[j] = vertices[j].normalized() * radius
+			uvs[j] = calculate_uv(vertices[j])
+			normals[j] = calculate_normals(vertices[j])
+			
+			colors.append(Color(0, 0.5, 0.5, 1.0))
+			
+ #			print(c)
+#			if i == 0:
+#				colors.append(Color(0, 0, c, 1.0)) # Generate a random color for each quad and append it to the colors array
+#			else:
+#				colors.append(Color(0, 0, 0, 1.0))
+
+		array[Mesh.ARRAY_VERTEX] = vertices
+		array[Mesh.ARRAY_TEX_UV] = uvs
+		array[Mesh.ARRAY_NORMAL] = normals
+		array[Mesh.ARRAY_COLOR] = PackedColorArray(colors)
+
+		mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
+	
+	var material = StandardMaterial3D.new()
+	material.vertex_color_use_as_albedo = true
+	
+	for i in range(surfaces):
+		mesh.surface_set_material(i, material)
+		
+	return mesh
+
+func calculate_uv(vertex):
+	var theta = atan2(vertex.z, vertex.x) # azimuthal angle
+	var phi = acos(vertex.y / Radius) # polar angle
+
+	var u = theta / (2.0 * PI) + 0.5
+	var v = phi / PI
+
+	return Vector2(u, v)
+
+func calculate_normals(vertex):
+	return vertex.normalized()
