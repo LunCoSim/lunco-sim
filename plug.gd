50bd7a364e5beb6106a5409fbfa844ceed7d8962
Difint <yarm-public@protonmail.com>
2023-09-21 16:15:39 +0700

    - Started integration with IVoyager

--- plug.gd
+++ plug.gd
@@ -25,3 +25,9 @@ func _plugging():
 	## Libraries
 	plug("maktoobgar/scene_manager") ## Scene manager
 	plug("PunchablePlushie/godot-game-settings") ## Game settings
+
+
+	## IVoyager integration
+	print("ivoyager_assets should be downloaded manually https://github.com/ivoyager/ivoyager/releases")
+	plug("ivoyager/ivoyager_table_importer", {"install_root": "addons/ivoyager_table_importer", "include": ["."]})
+	plug("ivoyager/ivoyager", {"install_root": "ivoyager", "include": ["."]})
