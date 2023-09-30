e7aaad423d20eb83ab43a73f5aca165184dbf4d6
Difint <yarm-public@protonmail.com>
2023-09-30 22:35:58 +0700

    - Refactored networking code, now player control is done in the Networking class

--- core/singletones/LCNetworking.gd
+++ core/singletones/LCNetworking.gd
@@ -1,13 +1,67 @@
+# This is a class for networking 
 class_name LCNetworking
 extends Node
 
-var peer = ENetMultiplayerPeer.new()
+# Declaration of peer as MultiplayerPeer, which will be used to handle multiplayer networking
+var peer: MultiplayerPeer
 
-## 
+# A dictionary to store the connected players
+var players = {}
+
+# Initialization function
+func _init():
+	# Creating a new ENetMultiplayerPeer
+	peer = ENetMultiplayerPeer.new()
+
+func _ready():
+	# Setting up signals to call relevant functions when a peer is connected or disconnected.
+	multiplayer.peer_connected.connect(on_peer_connected)
+	multiplayer.peer_disconnected.connect(on_peer_disconnected)
+
+	# Setting up signals to call relevant functions upon server connection status changes.
+	multiplayer.connection_failed.connect(on_server_connection_failed)
+	multiplayer.connected_to_server.connect(on_server_connected)
+	multiplayer.server_disconnected.connect(on_server_disconnected)
+
+# Function to connect to a server
 func connect_to_server(ip: String, port: int):
+	# Creating a client
 	peer.create_client(ip, port)
+	# Assigning the peer to this multiplayer's peer
 	multiplayer.multiplayer_peer = peer
-	
+
+# Function to start hosting a server
 func host(port: int = 9000):
-	peer.create_server(9000)
+	# Creating a server
+	peer.create_server(port)
+	# Assigning the peer to this multiplayer's peer
 	multiplayer.multiplayer_peer = peer
+
+
+#---------------------------------------------------
+
+# Function called when a peer connects
+func on_peer_connected(id):
+	# Adding the peer to players dictionary
+	players[id] = {}
+
+# Function called when a peer disconnects
+func on_peer_disconnected(id):
+	# Removing the peer from players dictionary
+	players.erase(id)
+
+
+# Function called when connection to server failed
+func on_server_connection_failed():
+	# This function currently does nothing.
+	pass
+
+# Function called when successfully connected to server.
+func on_server_connected():
+	# This function currently does nothing.
+	pass
+
+# Function called when server gets disconnected
+func on_server_disconnected():
+	# Printing a message to signal loss of server connection
+	print("Lost connection to server")
--- sim/sim.gd
+++ sim/sim.gd
@@ -1,82 +1,31 @@
 extends Node
 
 
-#
-var PlayerEntity := preload("res://core/entities/player-entity.tscn")
-var OperatorEntity := preload("res://core/entities/operator-entity.tscn")
-var SpacecraftEntity := preload("res://core/entities/starship-entity.tscn")
-
-
-var players = {}
 var entities = []
 
-func _init():
-	# Hack for compatibility with IVoyager
-	IVGlobal.settings.gui_size = 1
-	
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if multiplayer.is_server():
 		%MachineRole.text = "Server"
 		
-		multiplayer.peer_connected.connect(on_peer_connected)
-		multiplayer.peer_disconnected.connect(on_peer_disconnected)
 	else:
 		%MachineRole.text = "Peer id: " + str(multiplayer.get_unique_id())
 		
-		multiplayer.connection_failed.connect(on_server_connection_failed)
-		multiplayer.connected_to_server.connect(on_server_connected)
-		multiplayer.server_disconnected.connect(on_server_disconnected)
 	
 	
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Universe.position -= $Avatar.position
-	$Avatar.position = Vector3.ZERO
+	#$Universe.position -= $Avatar.camera_global_position()
+	#$Avatar.position = Vector3.ZERO
+	pass
 
-## Signal processing
 
+	
 #------------------------------------------------------
 
-func on_peer_connected(id):
-	print("player connected: ", id)
-	players[id] = {}
-	Entities.set_multiplayer_authority(id)
-
-func on_peer_disconnected(id):
-	print("player removed: ", id)
-	
-	players.erase(id)
-	
-	for entity in %SpawnPosition.get_children():
-		if entity.name.to_int() == id:
-			entity.queue_free()
-
-func on_server_connection_failed():
-	pass
-	
-func on_server_connected():
-	pass
-	
-func on_server_disconnected():
-	print("Lost connection to server")
-	
-#------------------------------------------------------
-	
-@rpc("call_local", "any_peer")
-func send_message(player_name, message, is_server):
-	pass
-
-@rpc("any_peer")
-func add_player(id):
-	pass
-#	var player_instance = player.instantiate()
-#	player_instance.name = str(id)
-#	%SpawnPosition.add_child(player_instance)
-
-#	send_message.rpc(str(id), " has joined the game", false)
 
 @rpc("any_peer", "call_local")
 func spawn(_entity: EntitiesDB.Entities): #TBD think of a class entity
@@ -99,18 +48,14 @@ func spawn(_entity: EntitiesDB.Entities): #TBD think of a class entity
 
 		_on_multiplayer_spawner_spawned(entity)
 
-		send_message.rpc(str(id), " has joined the game", false)
 	else:
 		var entity = Entities.make_entity(_entity)
-		
 
-#		entity.set_multiplayer_authority(id)
-		
 		%SpawnPosition.add_child(entity, true)
 
 		_on_multiplayer_spawner_spawned(entity)
 
-		send_message.rpc(str(id), " has joined the game", false)
+
 	
 #----------
 # Signals from Avatar
@@ -145,7 +90,7 @@ func _on_avatar_ray_cast(from: Vector3, to: Vector3):
 	if result:
 		
 		if result.collider is StaticBody3D:
-			var starship= SpacecraftEntity.instantiate()
+			var starship= Entities.make_entity(EntitiesDB.Entities.Spacecraft)
 			starship.position = $Universe.to_local(result.position) + Vector3(0, 1, 0)
 
 			$Universe.add_child(starship)
