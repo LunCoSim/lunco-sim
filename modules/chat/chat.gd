e26ff82047ac69a57eb61ba4f70a3131f4e9008e
Difint <yarm-public@protonmail.com>
2023-10-05 20:30:10 +0700

    - Introduced chat module

---
+++ modules/chat/chat.gd
@@ -0,0 +1,14 @@
+## This particular module would provide chat capabilitis:
+## 1. Window for chat + command/shortkey like Ctrl+T to show chat. 
+## 
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ modules/chat/module.gd
@@ -0,0 +1,4 @@
+## Here will be generic info about chat module for init
+extends Node
+
+
