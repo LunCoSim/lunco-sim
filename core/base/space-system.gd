0ba9e36c18ec88fa4b2faa20d0c9debd42346e9a
Difint <yarm-public@protonmail.com>
2023-10-06 14:19:47 +0700

    - Preparing to move to separate visual representation

--- core/base/space-system.gd
+++ core/base/space-system.gd
@@ -2,12 +2,15 @@
 class_name LCSpaceSystem
 extends Node3D #TODO: Check if should be node, maybe something else?
 
+@export var Visual: Resource
 #Basic parameters inspired by XTCE
+@export_group("XTCE")
 @export var Telemetry = {}
 @export var Parameters = {}
 @export var Commands = {}
 
 #----------------
+@export_group("Behaviour")
 @export var State = {} # Hierarchical state machine
 @export var Behaviour = {} # Behaviour tree
 
