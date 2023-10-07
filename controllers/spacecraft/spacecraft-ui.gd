1116f46a14810ec77b755d759734a93bc54a9de4
Difint <yarm-public@protonmail.com>
2023-10-07 12:53:13 +0700

    - Fixed Spaceship UI statisticks window

--- controllers/spacecraft/spacecraft-ui.gd
+++ controllers/spacecraft/spacecraft-ui.gd
@@ -1,8 +1,6 @@
 extends Control
 
-@export var Target: Node3D
-
-@onready var target = Target
+@onready var target: Node3D
 
 @onready var position_lbl = $"HUD/Position"
 @onready var direction_lbl = $"HUD/Direction"
@@ -16,36 +14,27 @@ var prev_velocity = Vector3.ZERO
 
 func set_target(_target):
 	target = _target
-	
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_UpdateUI_timeout():
-	if target:
+	print("_on_UpdateUI_timeout")
+	if target and target.Target: #target is controller and Target is the actual body
 		var Target = target.Target
-		if Target:
-			var vec = Target.transform.origin
-			position_lbl.text = "Position: (%.2f, %.2f, %.2f) Abs: %.2f" % [vec.x, vec.y, vec.z, vec.length()]
-			vec = Target.rotation
-			direction_lbl.text = "Orientation: (%.2f, %.2f, %.2f)" % [vec.x, vec.y, vec.z]
-			
-			vec = Target.linear_velocity
-			velocity_lbl.text = "Velocity: (%.2f, %.2f, %.2f) Abs: %.2f" % [vec.x, vec.y, vec.z, vec.length()]
-			
-			vec = Target.angular_velocity
-			angvelocity_lbl.text = "AngVelocity: (%.2f, %.2f, %.2f) Abs: %.2f" % [vec.x, vec.y, vec.z, vec.length()]
-			
-			#change 100 to delta
-			var acc = (Target.linear_velocity - prev_velocity) / 100
-			acceleration_lbl.text = "Acceleration: (%.2f, %.2f, %.2f) Abs: %.2f" % [acc.x, acc.y, acc.z, acc.length()]
-			prev_velocity = Target.linear_velocity
+		
+		var vec = Target.transform.origin
+		position_lbl.text = "Position: (%.2f, %.2f, %.2f) Abs: %.2f" % [vec.x, vec.y, vec.z, vec.length()]
+		vec = Target.rotation
+		direction_lbl.text = "Orientation: (%.2f, %.2f, %.2f)" % [vec.x, vec.y, vec.z]
+		
+		vec = Target.linear_velocity
+		velocity_lbl.text = "Velocity: (%.2f, %.2f, %.2f) Abs: %.2f" % [vec.x, vec.y, vec.z, vec.length()]
+		
+		vec = Target.angular_velocity
+		angvelocity_lbl.text = "AngVelocity: (%.2f, %.2f, %.2f) Abs: %.2f" % [vec.x, vec.y, vec.z, vec.length()]
+		
+		#change 100 to delta
+		var acc = (Target.linear_velocity - prev_velocity) / 100
+		acceleration_lbl.text = "Acceleration: (%.2f, %.2f, %.2f) Abs: %.2f" % [acc.x, acc.y, acc.z, acc.length()]
+		prev_velocity = Target.linear_velocity
 
 func _on_HideControls_timeout():
 	$Help.visible = false
