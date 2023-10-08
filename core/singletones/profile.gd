33d9246a215f780985c66ac82ae5287d59a49431
Difint <yarm-public@protonmail.com>
2023-10-09 01:11:53 +0700

    - Started to write down the idea of the profile

---
+++ core/singletones/profile.gd
@@ -0,0 +1,35 @@
+## This class represents user profile, and includes integration with web3
+# TBD implementation and integration with https://github.com/LunCoSim/lc-web3
+class_name LCProfile
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#-------------------------
+
+func login():
+	pass
+	
+func on_login_success():
+	pass
+	
+func get_perks():
+	pass
+	
+func on_perks_loaded():
+	pass
+
+func is_donor()->bool:
+	return false
+	
+func is_special_donor()->bool:
+	return false
+	
---
+++ docs/LunCo Profile.md
@@ -0,0 +1,3 @@
+LunCo will have integration with web3 via [lc-web3 addon.](https://github.com/LunCoSim/lc-web3)
+
+Different features would be tied to the profile - from possibility to change your avatar to publish you design online and tight it to it
