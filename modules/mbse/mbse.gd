6f8eef2fe3887ad9ce9ce4461c30eab67ce96be7
Difint <yarm-public@protonmail.com>
2024-01-11 09:03:36 +0700

    - Started to work on MBSE submodule

---
+++ modules/mbse/mbse.gd
@@ -0,0 +1,26 @@
+extends GraphEdit
+
+# LunCore:
+#  In:
+#  - Donations
+#  - Grants
+#  - Profile NFTS
+#  - Crowdfunding & Backers NFT
+#  - Services e.g. custom tokenomics model + tool for tokenomics
+
+#  Out:
+#   - compensations
+#   - marketing
+#   - operations (website, twitter, etc)
+
+#  Artefact:
+#   - LunCo
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
