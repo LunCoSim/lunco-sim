0722aeeba02a2c6d90c3d3d56756dbca0de58ff9
Difint <yarm-public@protonmail.com>
2024-12-10 15:54:54 +0700

    - Visualizing amount of resources

--- modules/supply_chain_modeling/base/base_resource.gd
+++ modules/supply_chain_modeling/base/base_resource.gd
@@ -10,6 +10,8 @@ var resource_id: String
 @export var volume: float = 0.0
 var custom_properties: Dictionary = {}
 var metadata: Dictionary = {}
+@export var current_amount: float = 100.0  # Current amount of resource available
+@export var max_amount: float = 100.0  # Maximum storage capacity
 
 func _init():
 	# Set up basic GraphNode properties
@@ -27,7 +29,8 @@ func _ready():
 	
 	# Set up basic appearance
 	size = Vector2(150, 80)  # Default size
-	
+	update_display()
+
 func set_resource_properties(id: String, desc: String, type: String):
 	resource_id = id
 	description = desc
@@ -62,9 +65,26 @@ func load_resource_data(data: Dictionary) -> void:
 		metadata = data.metadata 
 
 func remove_resource(amount: float) -> float:
-	# Implement logic to remove the specified amount of resource
-	# For example, you might want to track the current amount of resource available
-	# and return the amount that can actually be removed.
-	var amount_to_remove = min(amount, mass)  # Assuming mass represents the available amount
-	mass -= amount_to_remove
+	var amount_to_remove = min(amount, current_amount)
+	current_amount -= amount_to_remove
+	update_display()
 	return amount_to_remove
+
+func add_resource(amount: float) -> float:
+	var space_available = max_amount - current_amount
+	var amount_to_add = min(amount, space_available)
+	current_amount += amount_to_add
+	update_display()
+	return amount_to_add
+
+func update_display() -> void:
+	# Update the progress bar
+	var progress = $Properties/ProgressBar
+	if progress:
+		progress.max_value = max_amount
+		progress.value = current_amount
+		
+	# Update the amount label
+	var amount_label = $Properties/Amount
+	if amount_label:
+		amount_label.text = "Amount: %.2f / %.2f units" % [current_amount, max_amount]
