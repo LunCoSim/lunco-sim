0263d46580877d99efc265eef1854476ffe82599
Difint <yarm-public@protonmail.com>
2024-12-13 13:52:03 +0700

    - Connect New from menu

--- modules/supply_chain_modeling/menu_bar.gd
+++ modules/supply_chain_modeling/menu_bar.gd
@@ -1,5 +1,6 @@
 extends MenuBar
 
+signal new_graph_requested
 
 func _ready() -> void:
 	#hack as MenuBar by default uses PopupName of the node
@@ -7,3 +8,11 @@ func _ready() -> void:
 	
 	self.set_menu_title(0, "File")
 	self.set_menu_title(1, "NFT")
+	
+	# Connect the "New" menu item
+	%FileMenu.connect("id_pressed", _on_file_menu_pressed)
+
+func _on_file_menu_pressed(id: int) -> void:
+	match id:
+		0:
+			emit_signal("new_graph_requested")
