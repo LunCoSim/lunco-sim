b6bfc26edf1b6ca2a03a15c58794ae8ab004ef30
Difint <yarm-public@protonmail.com>
2025-02-03 14:51:14 +0700

    - Removed one more dependency

--- plug.gd
+++ plug.gd
@@ -16,7 +16,7 @@ func _plugging():
 
 	#Nodes & Behaviour
 	# plug("bitbrain/beehave") ## somehow it's not working properly, and as it's not used now - commented
-	plug("imjp94/gd-YAFSM")
+	# plug("imjp94/gd-YAFSM")
 	
 	#Developer Tools
 	# plug("Ark2000/PankuConsole")
