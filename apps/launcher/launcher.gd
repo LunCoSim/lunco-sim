72f3ac7fd41d0496be9d1d529658c4f337d0a1e9
Difint <yarm-public@protonmail.com>
2025-02-11 14:05:03 +0700

    - New launcher

--- apps/launcher/launcher.gd
+++ apps/launcher/launcher.gd
@@ -9,8 +9,13 @@ func _on_supply_chain_pressed():
 	get_tree().change_scene_to_file("res://apps/supply_chain_modeling/rsct.tscn")
 
 func _on_main_pressed():
-	# Load main scene
-	get_tree().change_scene_to_file("res://apps/3dsim/main.tscn")
+	# Check if running on web platform
+	if OS.has_feature("web"):
+		# Open 3D sim in new window if on web
+		OS.shell_open("https://alpha.lunco.space/3dsim/index.html")
+	else:
+		# Load main scene if not on web
+		get_tree().change_scene_to_file("res://apps/3dsim/main.tscn")
 
 func _on_main_animated_pressed():
 	# Load main animated scene
