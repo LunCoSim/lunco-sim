ec543014babb74c6a357aa4d5d849da5417d7257
Difint <yarm-public@protonmail.com>
2025-02-23 16:11:31 +0700

    - Fixes

--- apps/modelica_godot/ui/simulation_view.gd
+++ apps/modelica_godot/ui/simulation_view.gd
@@ -10,9 +10,11 @@ var dt: float = 0.01  # Time step
 @onready var simulation_world = $HSplitContainer/ViewportContainer/SubViewport/SimulationWorld
 
 var spring_visualization = null
+var spring_component_name: String = "Spring"
 
 func _ready() -> void:
 	equation_system = EquationSystem.new()
+	add_child(equation_system)
 	
 	k_spinbox.value_changed.connect(_on_k_value_changed)
 	length_spinbox.value_changed.connect(_on_length_value_changed)
@@ -21,42 +23,31 @@ func _ready() -> void:
 	spring_visualization = preload("res://apps/modelica_godot/ui/components/spring_visualization.tscn").instantiate()
 	simulation_world.add_child(spring_visualization)
 
-func _on_k_value_changed(value: float):
-	if equation_system:
-		# Update spring constant in the equation system
-		for component in equation_system.components:
-			if "Spring" in component.name:
-				component.set_parameter("k", value)
-				break
+func _on_k_value_changed(value: float) -> void:
+	if equation_system and equation_system.has_component(spring_component_name):
+		equation_system.set_component_parameter(spring_component_name, "k", value)
 
-func _on_length_value_changed(value: float):
-	if equation_system:
-		# Update rest length in the equation system
-		for component in equation_system.components:
-			if "Spring" in component.name:
-				component.set_parameter("l0", value)
-				break
+func _on_length_value_changed(value: float) -> void:
+	if equation_system and equation_system.has_component(spring_component_name):
+		equation_system.set_component_parameter(spring_component_name, "l0", value)
 
-func set_equation_system(system: EquationSystem):
+func set_equation_system(system: EquationSystem) -> void:
+	if equation_system:
+		equation_system.queue_free()
 	equation_system = system
-	if system:
+	add_child(equation_system)
+	if system and system.has_component(spring_component_name):
 		# Initialize UI with system values
-		for component in system.components:
-			if "Spring" in component.name:
-				k_spinbox.value = component.get_parameter("k")
-				length_spinbox.value = component.get_parameter("l0")
-				break
+		k_spinbox.value = system.get_component_parameter(spring_component_name, "k")
+		length_spinbox.value = system.get_component_parameter(spring_component_name, "l0")
 
-func _process(_delta):
-	if equation_system:
+func _process(_delta: float) -> void:
+	if equation_system and equation_system.has_component(spring_component_name):
 		# Update visualization based on equation system state
-		for component in equation_system.components:
-			if "Spring" in component.name:
-				var length = equation_system.variables.get(component.name + ".length", 1.0)
-				var force = equation_system.variables.get(component.name + ".force", 0.0)
-				spring_visualization.set_current_length(length)
-				spring_visualization.set_force(force)
-				break
+		var length = equation_system.variables.get(spring_component_name + ".length", 1.0)
+		var force = equation_system.variables.get(spring_component_name + ".force", 0.0)
+		spring_visualization.set_current_length(length)
+		spring_visualization.set_force(force)
 
 func get_simulation_world() -> Node2D:
 	return simulation_world 
@@ -75,7 +66,10 @@ func _on_run_button_pressed() -> void:
 
 func _on_reset_button_pressed() -> void:
 	time = 0.0
+	if equation_system:
+		equation_system.queue_free()
 	equation_system = EquationSystem.new()
+	add_child(equation_system)
 
 func _to_string() -> String:
 	var result = "SimulationView:\n"
