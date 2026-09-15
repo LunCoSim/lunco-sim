397691b5b6d58b39172036d580b9cf2d39e38ec3
Rod <rod@lunco.space>
2026-09-15 15:27:11 +0200

    use remote LunCoSim transform gizmo fork

--- vendor/mitchty-transform-gizmo/crates/transform-gizmo-bevy/src/prelude.rs
+++
@@ -1,3 +0,0 @@
-pub use transform_gizmo::prelude::*;
-
-pub use crate::{GizmoCamera, GizmoOptions, GizmoTarget, TransformGizmoPlugin};
--- vendor/mitchty-transform-gizmo/crates/transform-gizmo/src/prelude.rs
+++
@@ -1,9 +0,0 @@
-pub use crate::config::{GizmoConfig, GizmoDirection, GizmoMode, GizmoOrientation, GizmoVisuals};
-pub use crate::gizmo::{Gizmo, GizmoDrawData, GizmoInteraction, GizmoResult};
-
-pub use enumset::{EnumSet, enum_set};
-
-pub use mint;
-
-pub use ecolor::Color32;
-pub use emath::Rect;
